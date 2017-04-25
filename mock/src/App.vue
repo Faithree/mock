6a10c2c1d4fc604c48a22c936c609b42db94e966
Faithree <359790031@qq.com>
2017-04-25 21:06:22 +0800

    mock

---
+++ mock/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="app">
+    <div class="text-center">
+      <router-link to="/news">
+        <button type="button" class="btn  btn-default">news</button>
+      </router-link>
+      <router-link to="/dailies">
+        <button type="button" class="btn  btn-default">dailies</button>
+      </router-link>
+      <router-link to="/zalent">
+        <button type="button" class="btn  btn-default">zalent</button>
+      </router-link>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+    export default {
+        name: 'app',
+        data() {
+            return {}
+        },
+    }
+
+</script>
+
+<style>
+  .app {
+    max-width: 600px;
+    margin: 40px auto;
+  }
+
+  .app a {
+    margin: 0 10px;
+  }
+
+  .router-link-active button:focus {
+    background: #286090;
+    color: #fff;
+  }
+
+  .router-link-active button {
+    background: #286090;
+    color: #fff;
+  }
+
+</style>
---
+++ mock/src/components/dailies.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="panel panel-default table_mt20" v-if="Data">
+        <!-- Default panel contents -->
+        <div class="panel-heading">news mock</div>
+        <!-- Table -->
+        <table class="table">
+            <tr v-for="item in Data.others">
+                <td>{{item.id}}</td>
+                <td>{{item.name}}</td>
+                <td>{{item.color}}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'index',
+        data() {
+            return {
+                isOk: 1,
+                Data: null
+            }
+        },
+        mounted() {
+            this.$http.get('/api/dailies').then((res) => {
+                if (res.data.ok === this.isOk) {
+                    this.Data = (res.data.data)
+                }
+            })
+        }
+    }
+
+</script>
+
+<style>
+    .table_mt20 {
+        margin-top: 20px;
+    }
+
+</style>
